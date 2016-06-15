32556e5a00c559a9efb3ad1f5082bfb95618c28d
VideoLAN Buildbot <buildbot@videolan.org>
2016-06-15 18:32:07 +0200

    Remove empty func && install goto for sudo user also

--- install.sh
+++ install.sh
@@ -19,10 +19,6 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-function sudo_bashrc()
-{
-}
-
 function install()
 {
     local user_folder=$HOME;
@@ -39,14 +35,14 @@ function install()
     sudo cp goto /usr/bin/_goto;
     sudo cp goto.autocomplete /etc/bash_completion.d/goto;
     sudo cp goto_man /usr/local/share/man/man1/goto.1;
-    grep "alias goto=\"source _goto\"" $user_folder/.bashrc > /dev/null 2> /dev/null;
+    grep "alias goto=" $user_folder/.bashrc > /dev/null 2> /dev/null;
     if [ $? -ne 0 ]; then
         echo "alias goto=\"source _goto\"" >> $user_folder/.bashrc;
     fi;
-    # grep "alias goto=\"source _goto\"" ~root/.bashrc > /dev/null 2> /dev/null;
-    # if [ $? -ne 0 ]; then
-    #     echo "alias goto=\"source _goto\"" >> ~root/.bashrc;
-    # fi;
+    grep "alias goto=" ~root/.bashrc > /dev/null 2> /dev/null;
+    if [ $? -ne 0 ]; then
+        sudo tee ~root/.bashrc <<< "alias goto=\"source _goto\"" > /dev/null;
+    fi;
 }
 
 install;
